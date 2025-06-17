8187b6dea30e002729bb095a08adc530872a6b0e
Estimp <javierchiquitin02@gmail.com>
2025-06-17 23:37:10 +0200

    Initial commit

---
+++ src/main/java/com/estimp/breakify_service/BreakifyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.estimp.breakify_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakifyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakifyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estimp/breakify_service/BreakifyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estimp.breakify_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakifyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
