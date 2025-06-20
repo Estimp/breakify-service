635eb85885188bec08439909fe0cc22114c18405
Estimp <javierchiquitin02@gmail.com>
2025-06-20 21:00:25 +0200

    Added constructor to CreateAppDTO

--- src/main/java/com/estimp/breakify_service/model/dto/CreateAppDTO.java
+++ src/main/java/com/estimp/breakify_service/model/dto/CreateAppDTO.java
@@ -1,8 +1,10 @@
 package com.estimp.breakify_service.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CreateAppDTO {
     private String name;
 
